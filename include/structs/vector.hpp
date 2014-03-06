b96699208d55e20276b50b566adb9b717234b342
aoisensi <aoisensi@aoisensi.info>
2014-03-07 03:30:12 +0900

    const

--- include/structs/vector.hpp
+++ include/structs/vector.hpp
@@ -1,16 +1,19 @@
 #ifndef __HPP_WNM_VECTOR__
 #define __HPP_WNM_VECTOR__
 
+#include<cmath>
+
 namespace wnm {
     
     struct Vector3 {
+    
     public:
-        float x, y, z;
+        double x, y, z;
         
-        Vector3(float x, float y, float z) 
+        Vector3(double x = 0.0, double y = 0.0, double z = 0.0) 
         : x(x), y(y), z(z) { }
         
-        float Dot(Vector3 vec)
+        const double Dot(const Vector3 &vec) const
         {
             return
                 x * vec.x +
@@ -18,12 +21,53 @@ namespace wnm {
                 z * vec.z;
         }
         
-        Vector3 Cross(Vector3 vec)
+        Vector3 Cross(const Vector3 &vec) const
         {
             return Vector3(
                 y * vec.z - z * vec.y,
                 z * vec.x - x * vec.z,
                 x * vec.y - y * vec.x);
+        }
+    };
+
+    struct Vector2
+    {
+    
+    public:
+        double x, y;
+        
+        Vector2(const double x = 0.0, const double y = 0.0)
+        : x(x), y(y) { }
+        
+        const double Length() const
+        {
+            return std::sqrt(LengthSq());
+        }
+        
+        const double LengthSq() const
+        {
+            return x*x + y*y;
+        }
+        
+        Vector2 operator+(const Vector2 &value) const
+        {
+            return Vector2(
+                x + value.x,
+                y + value.y);
+        }
+        
+        Vector2 operator-(const Vector2 &value) const
+        {
+            return Vector2(
+                x - value.x,
+                y - value.y);
+        }
+        
+        Vector2 operator*(const double value) const
+        {
+            return Vector2(
+                x * value,
+                y * value);
         }
     };
 }
