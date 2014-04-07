52f187bda776ee1bc1bb06e9a289ee5d63bf9415
aoisensi <aoisensi@aoisensi.info>
2014-04-07 12:28:23 +0900

    fix?

---
+++ include/cl/render.hpp
@@ -0,0 +1,17 @@
+#ifndef _HPP_WNM_CL_RENDER_
+#define _HPP_WNM_CL_RENDER_
+
+#include <iostream>
+#include <list>
+#include <memory>
+
+using namespace std;
+
+namespace WeNeedMetal { namespace rd {
+
+	class Render {
+		list<weak_ptr<
+	};
+}}
+
+#endif
--- src/wnm/field.cpp
+++ src/wnm/field.cpp
@@ -4,5 +4,5 @@ using namespace WeNeedMetal::wnm;
 
 WeNeedMetal::wnm::Field::Field(const Vector3i& size) :
 	m_size(size),
-	m_terrain(boost::extents[m_size.x][m_size.y][m_size.z])
+	m_terrain(boost::extents[m_size.x][m_size.y])
 { };
